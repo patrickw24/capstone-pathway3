2d46ff764e8358bdadf0d2bcdeda87e98b5b41f9
patrickw24 <patrick.warner24@gmail.com>
2024-12-16 19:26:44 -0600

    HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { LoginWall } from './Components/LoginWall'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { ReportWall } from './Components/ReportWall'
-import { NavBar } from './ScreenComponents/Navbar'
-import { AddEmployee } from './Components/AddEmployee'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { LoginWall } from "./Components/LoginWall";
 
+import { ReportWall } from "./Components/ReportWall";
+import { NavBar } from "./ScreenComponents/Navbar";
+import { AddEmployee } from "./Components/AddEmployee";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-
-  let user= localStorage.getItem("username")
+  let user = localStorage.getItem("username");
 
   return (
     <>
-      <BrowserRouter>
-      <NavBar />
-      <Routes>
-      <Route path= "/" element={<LoginWall />}></Route>   
-      <Route path= "/report" element={<ReportWall username={user}/>}></Route> 
-      <Route path= "/form" element={<AddEmployee />}></Route> 
-
-      </Routes>
-      </BrowserRouter>
+      <HashRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<LoginWall />} />
+          <Route
+            path="/report"
+            element={<ReportWall username={user} />}
+          />
+          <Route path="/form" element={<AddEmployee />}></Route>
+        </Routes>
+      </HashRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
